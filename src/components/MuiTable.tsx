0c6c1f7a0de239d2921c818f4bbc3de99f241b8c
Doaa Albadri <doaabadri13@gmail.com>
2023-01-15 12:50:23 -0800

    make headers dynamic

--- src/components/MuiTable.tsx
+++ src/components/MuiTable.tsx
@@ -6,23 +6,21 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { TableProps } from '../pages/Users';
 import { IconButton } from '@mui/material';
+import { TableProps } from '../App';
 
 
-export default function MuiTable({ data }: TableProps) {
+export default function MuiTable({ data, tableHeaders, onClick }: TableProps) {
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>
-            </TableCell>
-            <TableCell align="center">Name</TableCell>
-            <TableCell align="center">Phone</TableCell>
-            <TableCell align="center">Address</TableCell>
-            <TableCell>
-            </TableCell>
+            {tableHeaders.map((header, index) => {
+              return (
+                <TableCell key={index} align="center">{header}</TableCell>
+              )
+            })}
           </TableRow>
         </TableHead>
         <TableBody>
@@ -38,7 +36,7 @@ export default function MuiTable({ data }: TableProps) {
               <TableCell align="center">{row.phone}</TableCell>
               <TableCell align="center">{row.address}</TableCell>
               <TableCell align="center">
-                <IconButton color='primary'>
+                <IconButton onClick={() => onClick(row)} color='primary'>
                   {row.btn}
                 </IconButton>
               </TableCell>
