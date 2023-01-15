92fab962752d030005512cbe47eb3583be38b261
Doaa Albadri <doaabadri13@gmail.com>
2023-01-15 12:50:10 -0800

    move data to app.tsx

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,54 +1,79 @@
 import MuiTable from "../../components/MuiTable"
-import harry from '../../assets/imgs/harrypotter.jpg'
-import ron from '../../assets/imgs/ronweasley.jpg'
-import hermoine from '../../assets/imgs/hermionegranger.jpg'
-import luna from '../../assets/imgs/lunalovegood.jpg'
-import neville from '../../assets/imgs/nevillenongbottom.jpg'
-import VisibilityIcon from '@mui/icons-material/Visibility';
 import Button from "@mui/material/Button"
+import { useState } from "react"
+import MuiModal from "../../components/MuiModal"
+import { TextField, Stack, Typography } from "@mui/material"
+import styles from './users.module.css'
+import { TableProps } from "../../App"
 
-export type TableItem = {
-    img: string,
-    name: string,
-    phone: number,
-    address: string,
-    btn: React.ReactElement
+export type ModalProps = {
+    open: boolean
+    handleOpen: () => void
+    handleClose: () => void
+    children: any
 }
 
-export type TableProps = {
-    data: TableItem[]
-}
 
+const Users = ({ tableHeaders, data, onClick }: TableProps) => {
 
-const Users = () => {
+    const [open, setOpen] = useState(false);
+    const [filebase64, setFileBase64] = useState<string>("")
 
-    function createData(
-        img: string,
-        name: string,
-        phone: number,
-        address: string,
-        btn: React.ReactElement
-    ) {
-        return { img, name, phone, address, btn };
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    function convertFile(files: FileList | null) {
+        if (files) {
+            const fileRef = files[0] || ""
+            const fileType: string = fileRef.type || ""
+            console.log("This file upload is of type:", fileType)
+            const reader = new FileReader()
+            reader.readAsBinaryString(fileRef)
+            reader.onload = (ev: any) => {
+                // convert it to base64
+                setFileBase64(`data:${fileType};base64,${btoa(ev.target.result)}`)
+            }
+        }
     }
-
-    const data = [
-        createData(harry, 'Harry Potter', 974635333, 'address', <VisibilityIcon />),
-        createData(ron, 'Ron Weasely', 345098976, 'address', <VisibilityIcon />),
-        createData(hermoine, 'Hermoine Granger', 262377747, 'address', <VisibilityIcon />),
-        createData(luna, 'Luna Lovegood', 305836799, 'address', <VisibilityIcon />),
-        createData(neville, 'Neville Longbottom', 356009876, 'address', <VisibilityIcon />),
-    ];
 
     return (
         <>
             <div className="d-flex justify-content-between align-items-center mb-4">
                 <h4>Users</h4>
-                <Button variant="contained">
+                <Button onClick={handleOpen} variant="contained">
                     Add User
                 </Button>
             </div>
-            <MuiTable data={data} />
+            <MuiTable tableHeaders={tableHeaders} data={data} onClick={onClick} />
+            <MuiModal open={open} handleOpen={handleOpen} handleClose={handleClose}>
+                <Stack gap={3}>
+                    <Typography variant="h6">
+                        Add New User
+                    </Typography>
+                    <TextField id="outlined-basic" label="Name" variant="outlined" />
+                    <TextField id="outlined-basic" label="Phone" variant="outlined" />
+                    <TextField id="outlined-basic" label="Address" variant="outlined" />
+                    <label className={styles.uploadButton} htmlFor='inputTag'>
+                        <Typography >
+                            Upload Image
+                        </Typography>
+                        <input className='d-none' id="inputTag" type="file" onChange={(e) => convertFile(e.target.files)} />
+                    </label>
+                    {(filebase64.indexOf("image/") > -1) &&
+                        <div className="d-flex justify-content-center">
+                            <img className={styles.image} src={filebase64} width={200} />
+                        </div>
+                    }
+                    <Stack className="d-flex justify-content-end" direction="row" gap={4}>
+                        <Button variant="contained">
+                            Add
+                        </Button>
+                        <Button variant="contained">Cancel</Button>
+
+                    </Stack>
+                </Stack>
+
+            </MuiModal>
 
         </>
     )
