fac00b246a26d46de01d00a29ebdf972be60491b
Doaa Albadri <doaabadri13@gmail.com>
2023-01-15 12:50:38 -0800

    create modal component

---
+++ src/components/MuiModal.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { ModalProps } from '../pages/Users';
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 500,
+    bgcolor: 'background.paper',
+    border: 'none',
+    borderRadius: '5px',
+    boxShadow: 24,
+    p: 4,
+};
+
+export default function MuiModal({ open, handleOpen, handleClose, children }: ModalProps) {
+
+    return (
+        <div>
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={open}>
+                    <Box sx={style}>
+                        {children}
+                    </Box>
+                </Fade>
+            </Modal>
+        </div>
+    );
+}
